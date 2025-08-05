182034d53ba67b6ffed8f8d428692eb0b69d56ff
MakiforDevelop <makifordevelop@gmail.com>
2025-08-05 21:18:11 +0800

    Initial Commit

---
+++ NekoTranscribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NekoTranscribe
+//
+//  Created by 千葉牧人 on 2025/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NekoTranscribe/NekoTranscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoTranscribeApp.swift
+//  NekoTranscribe
+//
+//  Created by 千葉牧人 on 2025/8/5.
+//
+
+import SwiftUI
+
+@main
+struct NekoTranscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
